fdef04f14e3a0828deb67bb3d112ce05359432ae
Julia Dmitrievna <jukhanova@inbox.ru>
2025-12-13 13:43:52 +0300

    бургер меню

---
+++ assets/js/menu.js
@@ -0,0 +1,7 @@
+const menuToggle = document.getElementById("menuToggle");
+const nav = document.getElementById("mainNav");
+
+menuToggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+    menuToggle.classList.toggle("active");
+});
